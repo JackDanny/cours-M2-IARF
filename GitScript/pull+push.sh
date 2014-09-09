f8e376fee64ba584fd51bf2a2e615a4dd3c22d6e
JackDanny <wedg@outlook.fr>
2014-09-09 23:00:05 +0200

    commit de test

---
+++ GitScript/pull+push.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# pause2.sh : appuyer sur une touche pour continuer un script
+
+#on se place dans le repertoire principal
+cd ..
+#on fait un pull pour mettre à jour
+git pull origin master
+
+#on ajoute tous les fichiers à l'index
+git add -A
+
+echo "Entrez un nom de commit"
+
+#on recupère le nom du commit saisi dans la variable nomCommit
+read -r nomCommit
+
+#on fait un commit pour valider les changements
+git commit -m "$nomCommit"
+
+#on envoie les changement
+git push origin master
+
+
