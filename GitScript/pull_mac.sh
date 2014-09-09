b0116fccd9c01487b3386b3f94cc31661b377653
Wedge <wedg@hotmail.fr>
2014-09-09 23:55:56 +0200

    ajout du script pour puller sur mon mac

---
+++ GitScript/pull_mac.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# pull_mac.sh : pour faire un pull depuis mon mac
+
+#on se place dans le repertoire principal
+cd /Users/Wedge/Desktop/M2/cours-M2-IARF
+
+#on fait un pull pour mettre à jour
+git pull origin master
+
